423840c7221daf5a89c1fdcd67cd075142a20f5e
Alexander Zubko <alexander.zubko@gmail.com>
2019-06-14 10:17:42 +0300

    Refactor courseinfo following the task

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -19,28 +19,37 @@ const Content = ({ parts }) => (
   </div>
 );
 
-const Total = ({ total }) => <p>Number of exercises {total}</p>;
+const Total = ({ parts }) => (
+  <p>
+    Number of exercises{" "}
+    {parts.reduce((prev, current) => prev + current.exercises, 0)}
+  </p>
+);
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7
+      },
+      {
+        name: "State of a component",
+        exercises: 14
+      }
+    ]
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content
-        parts={[
-          { name: part1, exercises: exercises1 },
-          { name: part2, exercises: exercises2 },
-          { name: part3, exercises: exercises3 }
-        ]}
-      />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
